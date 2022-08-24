b0a724f6cc5ba167d43a1629f8f82bf25306b9c8
cobular <22972550+Cobular@users.noreply.github.com>
2022-08-23 18:23:38 -0700

    Added email submission and validation

--- src/lib/components/email.svelte
+++ src/lib/components/email.svelte
@@ -1,25 +1,55 @@
 <script lang="ts">
-	let text: string = "";
+	let text: string = '';
 
 	let error: string | undefined = undefined;
 
-	function submitHandler(e: Event) {
-		console.log(text)
+	let success: boolean = false;
+
+	async function submitHandler(e: Event) {
+		console.log(text);
+
+		try {
+			const resp = await fetch(
+				'/api/emil?' +
+					new URLSearchParams({
+						email: text
+					}),
+				{
+					method: 'POST'
+				}
+			);
+
+			if (resp.ok) {
+				success = true;
+			} else {
+				error = await resp.text()
+				success = false
+			}
+		} catch (err) {
+			let message = 'Unknown Error';
+			if (err instanceof Error) message = err.message;
+			error = message;
+			success = false
+		}
+		console.error(error);
 	}
 </script>
 
-<form 
+<form
 	class="w-full max-w-[40ch] lg:max-w-[50ch] xl:max-w-[65ch]"
 	on:submit|preventDefault={submitHandler}
-	>
-	<input
-		bind:value={text}
-		type="email"
-		placeholder="Enter your email for updates"
-		class="w-full px-3 py-2 leading-tight text-gray-900 bg-transparent border-b border-gray-900 appearance-none font-nunito lg:text-2xl xl:text-4xl focus:shadow-outline"
-	/>
-
-	{#if error !== undefined}
-		<div>Please enter a valid email</div>
+>
+	{#if error === undefined && !success}
+		<input
+			bind:value={text}
+			type="email"
+			placeholder="Enter your email for updates"
+			class="w-full px-3 py-2 leading-tight text-gray-900 bg-transparent border-b border-gray-900 appearance-none font-nunito lg:text-2xl xl:text-4xl focus:shadow-outline"
+		/>
+	{:else if success}
+		<div><p class="text-center font-nunito">Thank you! Please keep an eye on your email for more info.</p></div>
+	{:else}
+		<div>Error encountered. Please reload and try again.</div>
+		<div class="max-h-[7ch] overflow-scroll">Error: {error}</div>
 	{/if}
 </form>
