a837e65945465bc1156d3edf3a3315c19c7c52be
cobular <22972550+Cobular@users.noreply.github.com>
2022-09-21 12:49:50 -0700

    Added recaptcha!

--- src/lib/components/email.svelte
+++ src/lib/components/email.svelte
@@ -1,42 +1,58 @@
 <script lang="ts">
+  import { Circle2, Circle } from 'svelte-loading-spinners'
+
 	let text: string = '';
 
 	let error: string | undefined = undefined;
 
 	let success: boolean = false;
 
+	let loading: boolean = false;
+
 	async function submitHandler(e: Event) {
-		console.log(text);
+		console.log("Submitted")
+		loading = true
+		grecaptcha.ready(function () {
+			console.log("recaptcha ready")
+			grecaptcha
+				.execute('6LdT9hkiAAAAAM1maiWvJtgGAKTBfSYorsZ-IBV3', { action: 'submit' })
+				.then(async function (token: string) {
+					console.log("Recaptcha Callback")
+					try {
+						const resp = await fetch(
+							'/api/email?' +
+								new URLSearchParams({
+									email: text,
+									token
+								}),
+							{
+								method: 'POST'
+							}
+						);
 
-		try {
-			const resp = await fetch(
-				'/api/email?' +
-					new URLSearchParams({
-						email: text
-					}),
-				{
-					method: 'POST'
-				}
-			);
-
-			if (resp.ok) {
-				success = true;
-			} else {
-				error = await resp.text()
-				success = false
-			}
-		} catch (err) {
-			let message = 'Unknown Error';
-			if (err instanceof Error) message = err.message;
-			error = message;
-			success = false
-		}
-		console.error(error);
+						if (resp.ok) {
+							success = true;
+							loading = false;
+						} else {
+							error = await resp.text();
+							success = false;
+							loading = false;
+						}
+					} catch (err) {
+						let message = 'Unknown Error';
+						if (err instanceof Error) message = err.message;
+						error = message;
+						success = false;
+						loading = false;
+					}
+					console.error(error);
+				});
+		});
 	}
 </script>
 
 <form
-	class="w-full max-w-[40ch] lg:max-w-[50ch] xl:max-w-[65ch]"
+	class="w-full max-w-[40ch] lg:max-w-[50ch] xl:max-w-[65ch] relative"
 	on:submit|preventDefault={submitHandler}
 >
 	{#if error === undefined && !success}
@@ -46,8 +62,20 @@
 			placeholder="Enter your email for updates"
 			class="w-full px-3 py-2 leading-tight text-gray-900 bg-transparent border-b border-gray-900 appearance-none font-nunito lg:text-2xl xl:text-4xl focus:shadow-outline"
 		/>
+		{#if loading }
+		<div class="absolute right-2 top-0 bottom-0 my-auto h-[80%]">
+			<div class="static">
+				<!-- <Circle size="60" color="#59C6D7"/> -->
+				<Circle2 size="30" colorOuter="#59C6D7" colorCenter="#AAD6A1" colorInner="#FDDE97"/>
+			</div>
+		</div>
+		{/if}
 	{:else if success}
-		<div><p class="text-center font-nunito">Thank you! Please keep an eye on your email for more info.</p></div>
+		<div>
+			<p class="text-center font-nunito">
+				Thank you! Please keep an eye on your email for more info.
+			</p>
+		</div>
 	{:else}
 		<div>Error encountered. Please reload and try again.</div>
 		<div class="max-h-[7ch] overflow-scroll">Error: {error}</div>
--- src/routes/api/email/+server.ts
+++ src/routes/api/email/+server.ts
@@ -3,16 +3,29 @@ import type { RequestEvent } from "./$types";
 import * as EmailValidator from 'email-validator';
 import { SERVICE_ACCT_EMAIL, PRIVATE_KEY } from '$env/static/private';
 import { GoogleSpreadsheet } from "google-spreadsheet";
+import { validateCaptcha } from '$lib/captchaValidator';
 
 const SHEET_ID = "1ANWHf_xTo6Fjv9LgMGJ48MqUS0xnj6PwYmE16zwW3ts"
 
 export async function POST({url}: RequestEvent): Promise<Response> {
+  console.log("Processing POST request")
   const email = url.searchParams.get("email");
-
-  console.log(email)
+  const token = url.searchParams.get("token");
+  console.log(token)
 
   if (email === null || !EmailValidator.validate(email)) {
     throw error(400, "Please provide a valid email")
+  }
+
+  if (token === null) {
+    throw error(400, "Please provide a recatpcha token")
+  } else {
+    const resp = await validateCaptcha(token);
+    console.log(resp)
+    // If we should fail...
+    if (resp.score < 0.6 || resp.action != "submit") {
+      throw error(500, "Internal server error")
+    }
   }
 
   const doc = new GoogleSpreadsheet(SHEET_ID);
