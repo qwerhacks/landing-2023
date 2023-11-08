c5f1af071591f9fc3a7abb3790c9ae7dc5d379bd
Mia Taylor <miastaylor1@gmail.com>
2023-11-07 23:51:56 -0800

    quick blurb

--- src/lib/components/hero.svelte
+++ src/lib/components/hero.svelte
@@ -6,8 +6,16 @@ import Email from './email.svelte';
 <div class="flex flex-col justify-center items-center gap-8 p-10 md:p-14 rounded-xl w-max lg:w-[60%] max-w-[85vw] md:max-w-[70vw]">
   <Logo />
   <Email />
+  <span class="text-center text-white text-5xl font-reactor7" style="text-shadow:#200b3a 4px 0px 0px,#200b3a -4px 0px 0px,#200b3a 0px 4px 0px,#200b3a 0px -4px 0px">the lgbtqia community? at MY hackathon???<br/>it's more likely than you think.</span>
 </div>
 
 <style>
-  
+  .outlined::after {
+    -webkit-text-fill-color: #200b3a;
+    color: red;
+    font-size: 4rem;
+    content: "the lgbtqia community? at MY hackathon???\nit's more likely than you think.";
+    position: absolute; 
+    left: 0;
+  }
 </style>
