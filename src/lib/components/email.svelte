1b25ed76008495f3f4916997da50588f37e29ac9
Julie <22972550+Cobular@users.noreply.github.com>
2023-11-08 13:29:33 -0800

    Fix up response formatting

--- src/lib/components/email.svelte
+++ src/lib/components/email.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import Arrow from "$lib/media/arrow.svg";
-  import { Circle2, Circle } from 'svelte-loading-spinners'
+	import Arrow from '$lib/media/arrow.svg';
+	import { Circle2, Circle } from 'svelte-loading-spinners';
 
 	let text: string = '';
 
@@ -11,14 +11,14 @@
 	let loading: boolean = false;
 
 	async function submitHandler(e: Event) {
-		console.log("Submitted")
-		loading = true
+		console.log('Submitted');
+		loading = true;
 		grecaptcha.ready(function () {
-			console.log("recaptcha ready")
+			console.log('recaptcha ready');
 			grecaptcha
 				.execute('6LdT9hkiAAAAAM1maiWvJtgGAKTBfSYorsZ-IBV3', { action: 'submit' })
 				.then(async function (token: string) {
-					console.log("Recaptcha Callback")
+					console.log('Recaptcha Callback');
 					try {
 						const resp = await fetch(
 							'/api/email?' +
@@ -64,34 +64,50 @@
 			class="w-full px-5 py-2 leading-tight text-gray-900 bg-white appearance-none font-reactor7 lg:text-2xl xl:text-4xl focus:shadow-outline"
 			style="border-radius:50px"
 		/>
-		{#if !loading }
-			<button type="submit" id="submit" class="px-3 py-3 w-[40px] h-[40px] sm:w-[50px] sm:h-[50px]" style="border-radius:100%;aspect-ratio:1/1;background:#05034a">
-				<img src={Arrow}/>
+		{#if !loading}
+			<button
+				type="submit"
+				id="submit"
+				class="px-3 py-3 w-[40px] h-[40px] sm:w-[50px] sm:h-[50px]"
+				style="border-radius:100%;aspect-ratio:1/1;background:#05034a"
+			>
+				<img src={Arrow} />
 			</button>
 		{:else}
-			<button disabled type="submit" id="submit" class="px-2 py-2" style="border-radius:100%;aspect-ratio:1/1;width:50px;height:50px;background:#05034a">
-				<Circle2 size="30" colorOuter="#fff" colorCenter="#fff" colorInner="#fff"/>
+			<button
+				disabled
+				type="submit"
+				id="submit"
+				class="px-2 py-2"
+				style="border-radius:100%;aspect-ratio:1/1;width:50px;height:50px;background:#05034a"
+			>
+				<Circle2 size="30" colorOuter="#fff" colorCenter="#fff" colorInner="#fff" />
 			</button>
 		{/if}
 	{:else if success}
 		<div>
-			<p class="text-center font-reactor7 text-whit text-2xl lg:text-5xl ">
+			<p class="text-center font-reactor7 text-white text-xl lg:text-5xl">
 				Thank you! Please keep an eye on your email for more info.
 			</p>
 		</div>
 	{:else}
-		<div>Error encountered. Please reload and try again.</div>
+		<div>
+			<p class="text-center font-reactor7 text-white text-xl lg:text-5xl">
+				Error encountered. Please reload and try again.
+			</p>
+		</div>
 		<div class="max-h-[7ch] overflow-scroll">Error: {error}</div>
 	{/if}
 </form>
 
 <style>
-	@import url(https://db.onlinewebfonts.com/c/3101d0058e2d7317cd5a5af7a1216ff3?family=Reactor7); 
+	@import url(https://db.onlinewebfonts.com/c/3101d0058e2d7317cd5a5af7a1216ff3?family=Reactor7);
 	input::placeholder {
 		text-align: center;
 		font-family: 'Reactor7', monospace;
 	}
-	input, button {
+	input,
+	button {
 		outline: solid 8px #05034a;
 	}
 </style>
